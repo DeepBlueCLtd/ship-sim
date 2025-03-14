0a1299d845bc271233eda23fe4878e3c15e5bbce
Ian Mayo <ian@planetmayo.com>
2025-03-14 18:06:30 +0000

    alternate source for dark mode maps

--- src/components/ShipMap.tsx
+++ src/components/ShipMap.tsx
@@ -26,9 +26,9 @@ export const ShipMap: React.FC<ShipMapProps> = ({ ships, displayedTrailLength = 
       style={{ height: '100vh', width: '100%' }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url={isDarkMode ? 
-          'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png' : 
+          'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png' : 
           'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         }
       />
